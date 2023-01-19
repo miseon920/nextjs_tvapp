cec4f44710a139ab2683b0dd6046c55752fce66f
kim mi seon <miseon920@gmail.com>
2023-01-19 09:30:11 +0900

    20230119

--- pages/tvs/[...params].js
+++ pages/tvs/[...params].js
@@ -3,11 +3,12 @@ import React, { useState } from 'react'
 import Seo from '../../components/Seo';
 import Image from 'next/image';
 import { useEffect } from 'react';
+import Link from 'next/link';
 
 const Detail = ({params,result}) => {
-  const router = useRouter();
+  //const router = useRouter();
     //const { id } = router.query //id를 가져오기 위해 이렇게 쓸 수 있음 -es6문법
-  const [title, id] = router.query.params || []; //title과 id를 담은 배열이라는 것을 알기 때문에 2개를 넣어준것
+  //const [title, id] = router.query.params || []; //title과 id를 담은 배열이라는 것을 알기 때문에 2개를 넣어준것
   //const { img } = router.query.image;
   /* 새로고침시 에러가 발생하는 이유는 서버가 프리랜더 되기 때문 router.query.params은 서버에서 배열이므로 || []를 추가해 준다.
     이부분은 csr만 해결되므로 console확인시 html이 비어있는것이 확인 됨/ 데이터빌드시 사용되는 getServerSideProps 를 이용한다.
@@ -19,20 +20,34 @@ const Detail = ({params,result}) => {
   */ 
   return (
     <div className='detail'>
-      <Seo title={title} />
+      <Seo title={result.name} />
       <ul>
-        <li>{title} </li>
-        <li>{id}</li>
+        <li>{result.name} </li>
+        <li>{result.id}</li>
         { result.poster_path &&
         <li> 
           <Image
-                src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
-                alt={title}
-                width={520}
-                height={500}
-          ></Image>        
-          </li>
-        }
+              src={`https://image.tmdb.org/t/p/w500/${result.poster_path}`}
+              alt={result.name}
+              width={520}
+              height={500}
+          ></Image>   
+          <div className="view">{result.overview}
+            <Link href={result.homepage} target="_blank">view</Link>
+          </div>
+          {result.genres.length > 0 &&
+            <div className="genres">
+              <h3>genres</h3>
+              <ol>
+                {result.genres.map(genre => (
+                  <li key={genre.id}>{genre.name}</li>
+                ))
+                }
+                </ol>
+            </div>
+          }
+        </li>
+      }
       </ul>
       {/* 
       <h4>
@@ -54,6 +69,29 @@ const Detail = ({params,result}) => {
         }
         .detail ul li:last-child{
           text-align: center;
+        }
+        .view{
+          padding:50px 0
+        }
+        .view a{
+          display:flex;
+          margin:15px auto 0;
+          background:red;
+          width:120px;
+          align-items: center;
+          justify-content: center;
+          padding:5px 0;
+          border-radius: 15px;
+        }
+        .genres h3{
+          color:red
+        }
+        .genres ol li{
+          display: inline-block;
+          font-size:0.8em
+        }
+        .genres ol li~li{
+          padding-left:10px;
         }
         `}
         </style>
