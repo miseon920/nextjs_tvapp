110706fdcaa99eb734f8839c56223f705a68cec4
kim mi seon <miseon920@gmail.com>
2023-01-19 16:45:02 +0900

    20230119

--- pages/tvs/[...params].js
+++ pages/tvs/[...params].js
@@ -104,7 +104,8 @@ export default Detail
 export async function getServerSideProps({ params: { params} }) { //server side context를 제공해주므로
    //sevver에 params이 생성됨, 유저에게 로딩을 보여주고 싶지 않고 seo에 친화적으로 만들기 위해 getServerSideProps 사용
   const id = params[1];
-  const result = await (await fetch(`${process.env.NEXT_PUBLIC_URL}/apis/tvs/${id}`)).json();
+  //const result = await (await fetch(`${process.env.NEXT_PUBLIC_URL}/apis/tvs/${id}`)).json();
+  const result = await (await fetch(`${process.env.API_URL}/tv/${id}?api_key=${process.env.API_KEY}`)).json();
     
   return {
     props: {
